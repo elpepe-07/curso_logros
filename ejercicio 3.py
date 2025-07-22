fb110c6854a624d614bcfae2588b26374e317b53
Kasane_teto <sb7757939@gmail.com>
2025-07-23 01:57:55 +0200

    Add files via upload

---
+++ ejercicio 3.py
@@ -0,0 +1,23 @@
+import random
+
+def numero_random():
+    numero_secreto= random.randint(1,100)
+    intentos = 0
+    adivinando = False
+    print("hola esto es el adivina : ")
+    print("adivina un numero de entre 1 y 100 ")
+    
+    while not adivinando:
+        try:
+            intento = int(input("ingresa tu intento: "))
+            intentos += 1
+            if intento < numero_secreto:
+                print("El numero es mayor sigue")
+            elif intento > numero_secreto:
+                print("El numero es menor. !sigue intentando¡")
+            else:
+                adivinando = True
+                print(f"felicidades ese es el numero en {intentos} intentos ")
+        except ValueError:
+            print("Por favor,  ingresa un numero valido")                
+numero_random()
