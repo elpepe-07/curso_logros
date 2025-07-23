43b728641350a31b1a778002379a7171441a84dd
Kasane_teto <sb7757939@gmail.com>
2025-07-23 12:00:29 -0400

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,6 @@
+def calcular_area_rectangulo(base, altura):
+    """Función que calcula el área de un rectángulo."""
+    return base * altura
+# Llamada a la función calcular_area_rectangulo
+area = calcular_area_rectangulo(5, 3)  # Calcula el área de un rectángulo de base 5 y altura 3
+print(f"El área del rectángulo es: {area}")  # Imprime: El área del rectángulo es: 15
