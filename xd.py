088eab8b56f1f1b4f66c87089c0ccfaa64885a1f
Kasane_teto <sb7757939@gmail.com>
2025-07-28 05:03:00 +0200

    Update xd.html

--- xd.py
+++ xd.py
@@ -1,31 +1,32 @@
-#course="python for beginners"
-#print("python" in course)
-#price=5
-#print(not price>10)
-
-#and(both)
-#or(at least one)
-#not(inverse)
-tempeture=31
-if tempeture>30:
-    print("its a hot day today")
-    print("drink plenty of water")
-elif tempeture>20:
-    print("its a nice day today")
-elif tempeture>10:
-    print("its a it cold")
-else:
-    print("its cold")
-print("done")
-
-# another exercise
-
-
-#weight=int(input("weight: "))
-#unit=input("(K)g or (L)bs: " )
-#if unit.upper()=="K":
-#    converted=weight/0.45
-#    print("Weight in lbs: "+str(converted))
-#else:
-#    converted=weight*0.45
-#    print("Weight in Kgs: "+str(converted)) 
+<body>
+  <h1>The Brown Bear</h1>
+  <div id="introduction">
+    <h2>About Brown Bears</h2>
+    <p>The brown bear (<em>Ursus arctos</em>) is native to parts of northern Eurasia and North America. Its conservation status is currently <strong>Least Concern</strong>.<br /><br /> There are many subspecies within the brown bear species, including the Atlas bear and the Himalayan brown bear.</p>
+    <h3>Species</h3>
+    <ul>
+      <li>Arctos</li>
+      <li>Collarus</li>
+      <li>Horribilis</li>
+      <li>Nelsoni (extinct)</li>
+    </ul>
+    <h3>Features</h3>
+    <p>Brown bears are not always completely brown. Some can be reddish or yellowish. They have very large, curved claws and huge paws. Male brown bears are often 30% larger than female brown bears. They can range from 5 feet to 9 feet from head to toe.</p>
+  </div>
+  <div id="habitat">
+    <h2>Habitat</h2>
+    <h3>Countries with Large Brown Bear Populations</h3>
+    <ol>
+      <li>Russia</li>
+      <li>United States</li>
+      <li>Canada</li>
+    </ol>
+    <h3>Countries with Small Brown Bear Populations</h3>
+    <p>Some countries with smaller brown bear populations include Armenia, Belarus, Bulgaria, China, Finland, France, Greece, India, Japan, Nepal, Poland, Romania, Slovenia, Turkmenistan, and Uzbekistan.</p>
+  </div>
+  <div id="media">
+    <h2>Media</h2>
+    <img src="https://content.codecademy.com/courses/web-101/web101-image_brownbear.jpg" alt="A Brown Bear"/>
+        <video src="https://content.codecademy.com/courses/freelance-1/unit-1/lesson-2/htmlcss1-vid_brown-bear.mp4" width="320" height="240" controls>Video not supported</video>
+  </div>
+</body>
